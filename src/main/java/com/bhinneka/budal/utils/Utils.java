748e09f80a545379e57aae33d2782b3377fcfab1
wuriyanto <wuriyanto48@yahoo.co.id>
2019-01-15 22:24:20 +0700

    move utils to utils package

--- src/main/java/com/bhinneka/budal/utils/Utils.java
+++ src/main/java/com/bhinneka/budal/utils/Utils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.bhinneka.budal;
+package com.bhinneka.budal.utils;
 
 import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
